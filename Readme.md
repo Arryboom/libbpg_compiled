e469a04ff1762c14ab431baa1216af9c11bd2e16
arryboom <888@988587.com>
2019-08-01 15:24:36 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,73 @@
+This the compile record for libbpg 0.9.8 on ubuntu 19.04
+
+BPG (Better Portable Graphics) is a new image format. Its purpose is to replace the JPEG image format when quality or file size is an issue. 
+official link:
+#https://bellard.org/bpg/ 
+
+
+sudo apt install libsdl-image1.2-dev
+sudo apt-get install libpng-dev
+sudo apt-get install libjpeg-dev
+sudo apt install cmake
+sudo apt install g++
+sudo apt install g++
+sudo apt install gc
+sudo apt install gcc
+sudo apt-get install cmake-curses-gui
+sudo apt install libnuma-dev
+sudo apt install yasm
+
+
+
+in MakeFile:
+add '-lnuma' numa library
+add '-Wl,--no-as-needed'
+eg,edit MakeFile in folder 
+
+```CFLAGS:=-Os -Wall -MMD -fno-asynchronous-unwind-tables -fdata-sections -ffunction-sections -fno-math-errno -fno-signed-zeros -fno-tree-vectorize -fomit-frame-pointer -Wl,--no-as-needed```
+```LDFLAGS+=-Wl,--gc-sections -Wl,--no-as-needed```
+```LIBS:=-lrt -lnuma```
+
+#x265
+you may need compile x265 support lib first:
+
+``sudo ./libbpg-0.9.8/x265/build/linux/make-Makefiles.bash``
+will get 10bit,8bit,12bit folder in /libbpg-0.9.8/x265/build/linux/,cp them to /libbpg-0.9.8/x265.out/
+
+then ``make``
+
+
+#SDL
+
+https://www.libsdl.org/release/SDL-1.2.15.tar.gz
+
+
+when compile got error like:
+```
+error: conflicting types for ‘_XData32’
+```
+
+To get a clean compile, edit **./src/video/x11/SDL_x11sym.h** around line 166 so it looks like this:  
+
+<div style="margin:20px; margin-top:5px">
+
+<div class="smallfont" style="margin-bottom:2px">Code:</div>
+
+<pre class="bbcodeblock" dir="ltr" style="
+		margin: 0px;
+		margin-right: -99999px;
+		padding: 3px;
+		border: 1px inset;
+		width: 98%;
+		height: 130px;
+		text-align: left;
+		overflow: auto">#if 0
+#ifdef LONG64
+SDL_X11_MODULE(IO_32BIT)
+SDL_X11_SYM(int,_XData32,(Display *dpy,register long *data,unsigned len),(dpy,data,len),return)
+SDL_X11_SYM(void,_XRead32,(Display *dpy,register long *data,long len),(dpy,data,len),)
+#endif
+#endif</pre>
+
+</div>
+
---
+++ libbpg-0.9.8/x265.out/8bit/CMakeFiles/cli.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/input/input.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/input/input.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/input/y4m.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/input/y4m.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/input/yuv.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/input/yuv.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/output/output.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/output/output.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/output/raw.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/output/raw.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/output/reconplay.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/output/reconplay.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/output/y4m.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/output/y4m.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/output/yuv.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/output/yuv.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/x265-extras.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/x265-extras.cpp.o"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/x265.cpp" "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/cli.dir/x265.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EXPORT_C_API=1"
+  "HAVE_INT_TYPES_H=1"
+  "HAVE_LIBNUMA"
+  "HIGH_BIT_DEPTH=0"
+  "X265_ARCH_X86=1"
+  "X265_DEPTH=8"
+  "X265_NS=x265"
+  "X86_64=1"
+  "__STDC_LIMIT_MACROS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/."
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/common"
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/source/encoder"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jjj/Desktop/libbpg-0.9.8/x265/build/linux/8bit/CMakeFiles/x265-shared.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
